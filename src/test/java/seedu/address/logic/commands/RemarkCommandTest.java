05832cb63d7217871841040d86f30ca6db4600cb
Yuvaraj0702 <yuvaraj408@gmail.com>
2022-09-15 21:44:10 +0800

    Add newline

---
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemarkCommandTest {
+
+    @Test
+    void execute() {
+    }
+
+}
